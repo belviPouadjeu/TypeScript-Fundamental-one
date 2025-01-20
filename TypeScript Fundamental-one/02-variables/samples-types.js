4edb412c968a8e284ac277dfba85d8a7ac536af6
belviPouadjeu <pbelvinard21@gmail.com>
2025-01-20 13:01:49 +0100

    Variables

---
+++ TypeScript Fundamental-one/02-variables/samples-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 90.5;
+var firstName = "Belvinard";
+var lastName = "Pouadjeu";
+console.log(found);
+console.log("The grade is " + grade);
+console.log("hi ".concat(firstName, " ").concat(lastName));
---
+++ TypeScript Fundamental-one/02-variables/samples-types.ts
@@ -0,0 +1,26 @@
+// A variable is a named container used to store data or information in a program.
+
+// Define a boolean variable to indicate whether something is found
+let found: boolean = true;
+// Define a number variable to store the grade of a student
+let grade: number = 90.5;
+
+// Define a string variable to store the first name of a person
+let firstName: string = "Belvinard";
+
+// Define a string variable to store the last name of a person
+let lastName: string = "Pouadjeu";
+
+
+//console.log("hi ".concat(firstName, " ").concat(lastName));
+
+// Print the value of the boolean variable to the console
+console.log(found);
+
+// Print a message to the console that includes the grade
+console.log("The grade is " + grade);
+
+// Print a greeting message using template literals that include the first and last name
+console.log(`Hi ${firstName} ${lastName}`);
+
+
